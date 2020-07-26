129d7a8d6873f32be943d70c2de7efdea738abc7
Alexander Ignatov <yalishanda@abv.bg>
2020-07-26 13:58:14 +0300

    Refactor structure

---
+++ Rimichka/App/AppReducer.swift
@@ -0,0 +1,54 @@
+//
+//  AppReducer.swift
+//  Rimichka
+//
+//  Created by Alexander Ignatov on 26.07.20.
+//  Copyright © 2020 Yalishanda. All rights reserved.
+//
+
+import Combine
+
+typealias AppStore = Store<AppState, AppAction, AppEnvironment>
+
+enum AppReducer {
+    static func reduce(
+        state: inout AppState,
+        action: AppAction,
+        environment: AppEnvironment
+    ) -> AnyPublisher<AppAction, Never> {
+        switch action {
+        
+        case .search(let query):
+            state.searchState = .loading
+            return environment.apiService
+                .rhymesForWord(query)
+                .map { list in
+                    list.map { fetched in
+                        fetched.asRhymePair(originalWord: query)
+                    }
+                }.map { .setSearchResults($0) }
+                .catch({ error -> Just<AppAction> in
+                    return Just(.failSearch(errorMessage: error.message))
+                }).eraseToAnyPublisher()
+            
+        case .failSearch(errorMessage: let message):
+            state.searchState = .failed(errorMessage: message)
+            
+        case .setSearchResults(let results):
+            state.searchState = .loaded(serchResults: results)
+            
+        case .loadPersistedFavorites:
+            state.favoriteRhymes = environment.dbService.fetchAllSaved()
+            
+        case .markRhyme(let pair):
+            state.favoriteRhymes.insert(pair)
+            environment.dbService.add(pair)
+            
+        case .unmarkRhyme(let pair):
+            state.favoriteRhymes.remove(pair)
+            environment.dbService.remove(pair)
+        }
+        
+        return Empty().eraseToAnyPublisher()
+    }
+}
--- Rimichka/RimichkaApp.swift
+++ Rimichka/RimichkaApp.swift
@@ -7,16 +7,13 @@
 //
 
 import SwiftUI
-import Combine
-
-typealias AppStore = Store<AppState, AppAction, AppEnvironment>
 
 @main
 struct RimichkaApp: App {
     
     private let store = AppStore(
         initialState: .init(),
-        reducer: RimichkaApp.reduce,
+        reducer: AppReducer.reduce,
         environment: .init()
     )
     
@@ -28,46 +25,5 @@ struct RimichkaApp: App {
     
     init() {
         store.send(.loadPersistedFavorites)
-    }
-    
-    private static func reduce(
-        state: inout AppState,
-        action: AppAction,
-        environment: AppEnvironment
-    ) -> AnyPublisher<AppAction, Never> {
-        switch action {
-        
-        case .search(let query):
-            state.searchState = .loading
-            return environment.apiService
-                .rhymesForWord(query)
-                .map { list in
-                    list.map { fetched in
-                        fetched.asRhymePair(originalWord: query)
-                    }
-                }.map { .setSearchResults($0) }
-                .catch({ error -> Just<AppAction> in
-                    return Just(.failSearch(errorMessage: error.message))
-                }).eraseToAnyPublisher()
-            
-        case .failSearch(errorMessage: let message):
-            state.searchState = .failed(errorMessage: message)
-            
-        case .setSearchResults(let results):
-            state.searchState = .loaded(serchResults: results)
-            
-        case .loadPersistedFavorites:
-            state.favoriteRhymes = environment.dbService.fetchAllSaved()
-            
-        case .markRhyme(let pair):
-            state.favoriteRhymes.insert(pair)
-            environment.dbService.add(pair)
-            
-        case .unmarkRhyme(let pair):
-            state.favoriteRhymes.remove(pair)
-            environment.dbService.remove(pair)
-        }
-        
-        return Empty().eraseToAnyPublisher()
-    }
+    }    
 }
